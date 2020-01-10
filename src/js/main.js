9b4c9bdd5daafbea453ff7009d6b5f130f209fed
Mirosław Malik <mmiro390@gmail.com>
2020-01-10 12:43:59 +0100

    add featch api and delete from html template static load repos

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,33 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
 console.log(`Hello world!`);
 
+const list=document.querySelector(".projects-container--js");
 
+fetch('https://api.github.com/users/bascommaster/repos?sort=updated&direction=desc')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos=resp;
+    for(const repo of repos){
+      const {description, html_url, name}=repo;
+      console.log(repo);
+      list.innerHTML+=`
+        <section class="project">
+          <ul class="project__list">
+            <li class="project-items">
+              <img src="../assets/img/github.svg" alt="" class="project-items__logo">
+              <h3 class="project-items__title">${name}.</h3>
+              <p class="project-items__description">${description}.</p>
+            </li>
+          </ul>
+          <div class="project-footer">
+            <a class="project-footer__link" href="#" title="Demo: ${name}."><i class="fa fa-desktop" aria-hidden="true"></i>Demo</a>
+            <a class="project-footer__link line" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="Source code: ${name}."><i class="fa fa-share-alt-square" aria-hidden="true"></i>GitHub</a>
+          </div>
+        </section>
+      `;
+    }
+  })
+  .catch(err=>{
+    console.log(err);
+  })

ee62cf3f7adb285a098806dd390db109ae5a166a
Mirosław Malik <mmiro390@gmail.com>
2020-01-10 13:59:06 +0100

    refactor generate html code from javascriipt, improve recent project style

--- src/js/main.js
+++ src/js/main.js
@@ -9,20 +9,25 @@ fetch('https://api.github.com/users/bascommaster/repos?sort=updated&direction=de
   .then(resp => {
     const repos=resp;
     for(const repo of repos){
-      const {description, html_url, name}=repo;
+      const {description, homepage, html_url, name}=repo;
       console.log(repo);
       list.innerHTML+=`
         <section class="project">
-          <ul class="project__list">
-            <li class="project-items">
-              <img src="../assets/img/github.svg" alt="" class="project-items__logo">
-              <h3 class="project-items__title">${name}.</h3>
-              <p class="project-items__description">${description}.</p>
-            </li>
-          </ul>
+          <div class="project-items">
+            <img src="../assets/img/github.svg" alt="" class="project-items__logo">
+            <h3 class="project-items__title">${name}</h3>
+            ${
+              description ? `<p class="project-items__description">${description}</p>` :`<p class="project-items__description">There is no description for this project 🙂</p>`
+            }
+            
+          </div>
+     
           <div class="project-footer">
-            <a class="project-footer__link" href="#" title="Demo: ${name}."><i class="fa fa-desktop" aria-hidden="true"></i>Demo</a>
-            <a class="project-footer__link line" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="Source code: ${name}."><i class="fa fa-share-alt-square" aria-hidden="true"></i>GitHub</a>
+            ${
+              homepage ? `<a class="project-footer__link" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}"><i class="fa fa-desktop" aria-hidden="true"></i>Demo</a>` : ''
+            }
+            
+            <a class="project-footer__link line" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="Source code: ${name}"><i class="fa fa-share-alt-square" aria-hidden="true"></i>GitHub</a>
           </div>
         </section>
       `;
